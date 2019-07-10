d63645fc931eb89da872fdf6118f947f813872ec
Ricardo Vieira <rica98@sapo.pt>
2019-07-11 00:12:00 +0100

    Projeto load

---
+++ Loja/Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Loja.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+        public DateTime DataDeAdquiricao { get; set; }
+        public DateTime DataDeVenda{ get; set; }
+    }
+}
