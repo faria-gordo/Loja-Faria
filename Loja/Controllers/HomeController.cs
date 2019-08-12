a6930bdcfc2bd2cf1e1a2f14d4a529611f67706c
Ricardo Vieira <rica98@sapo.pt>
2019-08-12 17:22:41 +0100

    Add files via upload

---
+++ Loja/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Loja.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Us()
+        {
+            return View();
+        }
+        public ActionResult Store(string productType)
+        {
+            if(productType == "")//Um dos tipos de artigos
+            {
+                //Conectar blob storage, retirar imagens
+                List<string> imagesPaths = new List<string>();
+                ViewBag.ProductName = productType.Trim().ToLower();
+                ViewBag.ImagesPaths = imagesPaths;
+            }
+            ViewBag.ProductType = productType;
+            //Colar productType a viewBag e na view() por partials
+            return View();
+        }
+    }
+}
