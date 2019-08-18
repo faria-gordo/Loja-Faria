81ed1e3349d01f0065af3d32cc5bf0ba44bb74b5
Ricardo Vieira <rica98@sapo.pt>
2019-08-18 18:47:11 +0100

    Deve estar tudo em ordem

---
+++ Loja/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Loja.Controllers
+{
+    public class ProductController : Controller
+    {
+        //Em vez de string ira haver o modelo de product com url, descricao, todos os campos do excel
+        public ActionResult Index(string product)
+        {
+            return View();
+        }
+    }
+}
