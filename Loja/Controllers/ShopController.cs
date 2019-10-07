9a1ba7f1697156a79fa74d19531afff2909ea74f
Ricardo Vieira <rica98@sapo.pt>
2019-10-08 00:13:08 +0100

    Up to date

--- Loja/Controllers/ShopController.cs
+++ Loja/Controllers/ShopController.cs
@@ -13,18 +13,21 @@ namespace Loja.Controllers
         [HttpPost]
         public ActionResult Index(string productName, string quantity)
         {
-            //Hold these values!! how?? session variables!! 
-            string partitionKey = productName.Split('-')[1] + productName.Split('-')[2];
+            //TODO: Aprender a usar session variables, aplicar a lista de produtos como session variable, sendo assim, caso seja adicionado algum produto
+            //      ao carrinho de compras ira aparecer na mesma sem apagar o ultimo
+            string partitionKey = PartitionKeyFormatter(productName);
             Loja.Data.Data manager = new Loja.Data.Data();
-            List<Produto> produtosCarrinho = new List<Produto>();
-            Produto produto = (from s in manager.Selecionar(partitionKey)
-                               where s.Nome == productName.Split('-')[0]
-                               select s).First();
+            List<Produto> produtosCarrinho = (from s in manager.Selecionar(partitionKey)
+                                              where s.Nome == productName.Split('-')[0]
+                                              select s).ToList<Produto>();
             ViewBag.Quantidade = quantity;
-            ViewBag.Produto = produto;
-            //Criar varias partials views para criar a pagina de carrinho
-            //Uma terá um iterador para a lista produtos
+            ViewBag.Produtos = produtosCarrinho;
             return View();
+        }
+        private string PartitionKeyFormatter(string uneditedPK)
+        {
+            string editedPK = uneditedPK.Split('-')[1].First() + "-" + uneditedPK.Split('-')[2].Substring(0, 3);
+            return editedPK;
         }
     }
 }
